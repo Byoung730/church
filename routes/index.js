9d2f1ac8243a6806a0a6ff9ad431d1ae7202eb11
byoung730 <brian.young@monsanto.com>
2018-09-07 14:28:08 -0500

    routes updated

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,17 @@ router.get("/", function(req, res, next) {
 router.get("/api/members", db.getAllMembers);
 router.get("/api/members/:member_id", db.getSingleMember);
 router.get("/api/debits", db.getAllDebits);
+router.get("/api/debits/:transaction_id", db.getSingleDebit);
 router.get("/api/credits", db.getAllCredits);
+router.get("/api/credits/:transaction_out_id", db.getSingleCredit);
 router.post("/api/members", db.createMember);
+router.post("/api/debits", db.createDebit);
+router.post("/api/credits", db.createCredit);
 router.put("/api/members/:member_id", db.updateMember);
+router.put("/api/debits/:transaction_id", db.updateDebit);
+router.put("/api/credits/:transaction_out_id", db.updateCredit);
+router.delete("/api/debits/:transaction_id", db.removeDebit);
+router.delete("/api/credits/:transaction_out_id", db.removeCredit);
 router.delete("/api/members/:member_id", db.removeMember);
 
 module.exports = router;

b95381177f44b7636f2a6a237aa475f5512501fd
byoung730 <brian.young@monsanto.com>
2018-09-07 15:41:36 -0500

    blog routes added

--- routes/index.js
+++ routes/index.js
@@ -21,5 +21,10 @@ router.put("/api/credits/:transaction_out_id", db.updateCredit);
 router.delete("/api/debits/:transaction_id", db.removeDebit);
 router.delete("/api/credits/:transaction_out_id", db.removeCredit);
 router.delete("/api/members/:member_id", db.removeMember);
+router.get("/api/blogs", db.getAllBlogs);
+router.get("/api/blogs/:blog_id", db.getSingleBlog);
+router.post("/api/blogs", db.createBlog);
+router.put("/api/blogs/:blog_id", db.updateBlog);
+router.delete("/api/blogs/:blog_id", db.removeBlog);
 
 module.exports = router;
